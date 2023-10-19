8e84180ac342048d51663d08b1c04dddb2b3e414
ZmnRobin <robinmilford38@gmail.com>
2023-10-19 08:44:37 +0600

    added all file

---
+++ src/assets/data/faqs.js
@@ -0,0 +1,32 @@
+export const faqs = [
+  {
+    question: "What types of legal services do you provide?",
+    content:
+      "We offer a wide range of legal services, including but not limited to business law, tax law, immigration law, family law, labor law, and estate planning. Our team of experienced lawyers is here to help you with your legal needs.",
+  },
+  {
+    question: "How can I consult with one of your lawyers?",
+    content:
+      "You can schedule a consultation with one of our lawyers by contacting our office. We offer both in-person and virtual consultations to accommodate your needs.",
+  },
+  {
+    question: "Do you handle immigration cases?",
+    content:
+      "Yes, we specialize in immigration law. Our immigration lawyers can assist you with matters related to living and working in Bangladesh legally, including political refugees and asylum seekers.",
+  },
+  {
+    question: "What can a family lawyer assist me with?",
+    content:
+      "Our family lawyers are experienced in all areas of domestic relations and family-related issues. Whether you need assistance with divorce, child custody, or other family matters, we can provide expert legal advice and support.",
+  },
+  {
+    question: "What is the role of a business lawyer?",
+    content:
+      "A business lawyer is essential for handling a variety of legal matters related to business operations. They can help with issues such as liability, intellectual property disputes, mergers, and legal documentation revisions.",
+  },
+  {
+    question: "How can a tax lawyer help me or my business?",
+    content:
+      "Tax lawyers can provide guidance on complex tax rules and regulations, ensuring that individuals and businesses stay compliant with local and federal tax laws. They help with tax planning, audits, and other tax-related matters.",
+  },
+];
